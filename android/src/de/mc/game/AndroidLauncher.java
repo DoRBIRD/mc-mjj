17348c63c1e7f8faf65c4a98b54b7a82fa3acbed
Moritz Ellmers <mellmers@stud.hs-bremen.de>
2016-03-28 18:32:45 +0200

    - enabled Accelerometer and disabled compass

--- android/src/de/mc/game/AndroidLauncher.java
+++ android/src/de/mc/game/AndroidLauncher.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import de.mc.game.MainClass;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainClass(), config);
+		config.useAccelerometer = true;
+		config.useCompass = false;
+		initialize(new McGame(), config);
 	}
 }
