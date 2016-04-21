cc8b2745c5896ca8b6859e40bf3b22458bc89bb3
Jonas <jonas@oja.de>
2016-04-21 12:40:07 +0200

    tiled map wip

--- core/src/de/mc/game/Constants.java
+++ core/src/de/mc/game/Constants.java
@@ -1,6 +1,11 @@
 package de.mc.game;
 
+import com.badlogic.gdx.Gdx;
+
 public final class Constants {
-    public static final int WIDTH = 1080;
-    public static final int HEIGHT = 1920;
+    public static final float w = Gdx.graphics.getWidth();
+    public static final float h = Gdx.graphics.getHeight();
+
+    public static final float WIDTH = 1080;
+    public static final float HEIGHT = WIDTH  * (h / w);
 }
--- core/src/de/mc/game/views/GameScreen.java
+++ core/src/de/mc/game/views/GameScreen.java
@@ -73,17 +73,11 @@ public class GameScreen extends CustomScreenAdapter {
 
 
 		//WIP MAP
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, mcGame.width, mcGame.height);
+		camera.setToOrtho(false, Constants.WIDTH, Constants.WIDTH);
 		//camera.setToOrtho(false,w,h);
 		camera.update();
-		tiledMap = new TmxMapLoader().load("maps/map1.tmx");
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap,3F);
-
-		timerBarrier = 0;
-		timerScore = 0;
+		tiledMap = new TmxMapLoader().load("maps/Map-v1.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap,2f/3f);
 
 		gameState = GAME_READY;
 	}
@@ -91,10 +85,6 @@ public class GameScreen extends CustomScreenAdapter {
 	@Override
 	public void render (float delta) {
 		super.render(delta);
-
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
