7c555e9c3ee8dc8932f8b2a1bbdbb647acc80ccb
Moritz Ellmers <moritz.ellmers@web.de>
2016-05-19 14:17:10 +0200

    - velocity changing when user tilt the phone on x axis - score is now player position in m

--- core/src/de/mc/game/McGame.java
+++ core/src/de/mc/game/McGame.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -18,9 +17,6 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.utils.I18NBundle;
-
-import java.util.Locale;
 
 import de.mc.game.views.GameScreen;
 import de.mc.game.views.MainMenuScreen;
@@ -28,53 +24,28 @@ import de.mc.game.views.MainMenuScreen;
 public class McGame extends Game implements InputProcessor {
 
     public SpriteBatch batch;
-    public GlyphLayout glyphLayout;
-    public BitmapFont droidSansSmall;
-    public BitmapFont droidSansMedium;
-    public BitmapFont droidSansLarge;
     public FreeTypeFontGenerator generator;
     public FreeTypeFontGenerator.FreeTypeFontParameter parameter;
-    public Screen currentScreen;
-    public Screen mainMenuScreen;
-    public Screen gameScreen;
+    public Screen
+            currentScreen,
+            mainMenuScreen,
+            gameScreen;
     public AssetManager assetManager;
-    public I18NBundle languageStrings;
-    public float width;
-    public float height;
     public InputMultiplexer inputMultiplexer;
 
     public TextButton.TextButtonStyle defaultTextButtonStyle;
 
     public void create() {
         batch = new SpriteBatch();
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-        width = w;
-        height = h;
 
         inputMultiplexer = new InputMultiplexer(this);
         Gdx.input.setInputProcessor(inputMultiplexer);
         Gdx.input.setCatchBackKey(true);
 
-        // init fonts
-        glyphLayout = new GlyphLayout();
-
-        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/DroidSans.ttf"));
-
-        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 50;
-        droidSansSmall = generator.generateFont(parameter);
-
-        parameter.size = 100;
-        droidSansMedium = generator.generateFont(parameter);
-
-        parameter.size = 150;
-        droidSansLarge = generator.generateFont(parameter);
+        // init Constants
+        new Constants();
 
         loadDefaultStyles();
-
-        Locale locale = new Locale("de", "en");
-        languageStrings = I18NBundle.createBundle(Gdx.files.internal("strings/strings"), locale);
 
         assetManager = new AssetManager();
 
@@ -91,9 +62,6 @@ public class McGame extends Game implements InputProcessor {
 
     public void dispose() {
         batch.dispose();
-        droidSansSmall.dispose();
-        droidSansMedium.dispose();
-        droidSansLarge.dispose();
         generator.dispose();
         assetManager.dispose();
     }
@@ -106,7 +74,7 @@ public class McGame extends Game implements InputProcessor {
         textButtonStyle.up = skin.getDrawable("button");
         textButtonStyle.over = skin.getDrawable("button_pressed");
         textButtonStyle.down = skin.getDrawable("button_pressed");
-        textButtonStyle.font = droidSansMedium;
+        textButtonStyle.font = Constants.DROID_SANS_MEDIUM;
         textButtonStyle.fontColor = Color.BLACK;
 
         defaultTextButtonStyle = textButtonStyle;
